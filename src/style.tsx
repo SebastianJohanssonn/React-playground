2cc98129a73b1155dcc129e6b67cac3a9a2812b3
Sebastian Johansson <sebben.97@hotmail.com>
2019-04-01 13:24:19 +0200

    added style.

---
+++ src/style.tsx
@@ -0,0 +1,11 @@
+import { CSSProperties } from "react";
+
+export const centeredContent: CSSProperties = {
+    width: '100%',
+    height:'100%',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center"
+}
