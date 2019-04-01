9309ae916df63208ad1d9e849e782722b5afb131
Sebastian Johansson <sebben.97@hotmail.com>
2019-04-01 14:34:28 +0200

    made header.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,12 @@
 import React from "React";
+import Header from "./layout";
 
 export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return <h1 style = {centeredContent}>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
+export default function App(){
+    return <Header/>;
 }
 
 const centeredContent: React.CSSProperties = {
